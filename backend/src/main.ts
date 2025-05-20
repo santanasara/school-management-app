5b8548ced4d1e461b3a04585779b2ba23548bab4
Joan Medeiros <joanmedeiros2018@gmail.com>
2025-05-19 23:34:38 -0300

    git commit -m "chore: ajusta configurações do ESLint e Prettier para evitar conflitos de endOfLine" #2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-    const config = new DocumentBuilder()
+  const config = new DocumentBuilder()
     .setTitle('School Management API')
     .setDescription('API for a school management app')
     .setVersion('1.0')
