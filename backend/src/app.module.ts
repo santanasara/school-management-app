6a4bdac80681cd3c56bdbe744ad3be852d72a2fa
Joan Medeiros <joanmedeiros2018@gmail.com>
2025-05-20 20:54:50 -0300

    feat: configura conexão do backend com o banco usando TypeORM (#2)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const DB_PORT: number = parseInt(process.env.DATABASE_PORT || '5432', 10);
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: DB_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
