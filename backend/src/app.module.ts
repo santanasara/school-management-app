d659fe5f356dc105a898684d2eaefaf085f9e619
Jerffeson Gomes <jerffesongd@gmail.com>
2025-05-23 10:52:15 -0300

    feat: #3 - Adicionando suporte a multiplos ambientes profiles

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+
+function getEnvFilePath(): string {
+  switch (process.env.NODE_ENV) {
+    case 'production':
+      return '.env.prod';
+    case 'development':
+      return '.env';
+    case 'debug':
+      return '.env.debug';
+    default:
+      return '.env'; // fallback
+  }
+}
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: getEnvFilePath(),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
