46e3fafcee40d405718ae49217a7de5433a8b8d9
Joan Medeiros <joanmedeiros2018@gmail.com>
2025-05-23 19:12:22 -0300

    refactor(app): implementa layout com menu lateral e roteamento (#2)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
-import { CursoService } from '../app/services/curso.service';
-import { FormsModule } from '@angular/forms';
-import { CursoComponent } from './components/curso/curso.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CursoComponent],
-  template: `<app-curso></app-curso>`
+  imports: [
+    RouterModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-
-
 export class AppComponent {}
-
