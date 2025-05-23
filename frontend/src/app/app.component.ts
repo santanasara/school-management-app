67c668c60d58f302b1da0c2edd1da64269ed286a
Joan Medeiros <joanmedeiros2018@gmail.com>
2025-05-23 19:23:00 -0300

    feat(frontend): adicionar menu principal com Angular Material e configurar rota home básica (#2)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -14,10 +12,8 @@ import { RouterModule } from '@angular/router';
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    MatIconModule,
-    MatButtonModule
   ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {}
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
+import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes),
-    provideAnimations()
+    provideRouter(routes)
   ]
-});
+}).catch(err => console.error(err));
