839dedc3e6769f0cfa488a3fb2d2c20a5169ce17
Cleiane Bondade <clei.bondade@gmail.com>
2025-05-24 14:05:02 -0300

    Ajusta turma para ser uma entidade do typeorm

--- backend/src/turma/turma.module.ts
+++ backend/src/turma/turma.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TurmaService } from './turma.service';
 import { TurmaController } from './turma.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Turma } from './entities/turma.entity';
+import { UsuarioModule } from 'src/usuario/usuario.module';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Turma]), UsuarioModule],
   controllers: [TurmaController],
   providers: [TurmaService],
 })
--- backend/src/turma/turma.service.ts
+++ backend/src/turma/turma.service.ts
@@ -1,26 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTurmaDto } from './dto/create-turma.dto';
 import { UpdateTurmaDto } from './dto/update-turma.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Turma } from './entities/turma.entity';
 
 @Injectable()
 export class TurmaService {
+
+  constructor(
+    @InjectRepository(Turma)
+    private readonly turmaRepository: Repository<Turma>,
+  ) {}
+  
   create(createTurmaDto: CreateTurmaDto) {
-    return 'This action adds a new turma';
+    const novaTurma = this.turmaRepository.create(createTurmaDto);
+    return this.turmaRepository.save(novaTurma);
   }
 
   findAll() {
-    return `This action returns all turma`;
+    return this.turmaRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} turma`;
+    return this.turmaRepository.findOneBy({ id });
   }
 
   update(id: number, updateTurmaDto: UpdateTurmaDto) {
-    return `This action updates a #${id} turma`;
+    return this.turmaRepository.update(id, updateTurmaDto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} turma`;
+    return this.turmaRepository.delete(id);
   }
 }
