e122b834c45b049034089f424f4487d0b23c3853
Joan Medeiros <joanmedeiros2018@gmail.com>
2025-05-24 21:45:57 -0300

    feat: cria o component curso-create (#2)

---
+++ frontend/src/app/modules/curso/components/curso-create/curso-create.component.ts
@@ -0,0 +1,47 @@
+// src/app/modules/curso/components/curso-create/curso-create.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms'; // Para formulários reativos
+import { Router } from '@angular/router'; // Para navegação após a criação
+import { CursoService } from '../../services/curso.service';
+import { Curso } from '../../curso'; 
+
+@Component({
+  selector: 'app-curso-create',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule // Necessário para usar FormGroup, FormControl, Validators
+  ],
+  templateUrl: './curso-create.component.html',
+  styleUrls: ['./curso-create.component.css']
+})
+export class CursoCreateComponent implements OnInit {
+  cursoForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder, // Injeta FormBuilder para construir o formulário
+    private cursoService: CursoService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.cursoForm = this.fb.group({
+      nome: ['', Validators.required],
+      cargaHoraria: ['', [Validators.required, Validators.min(1)]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.cursoForm.valid) {
+      const novoCurso: Curso = this.cursoForm.value;
+      this.cursoService.createCurso(novoCurso).subscribe(() => {
+        console.log('Curso criado com sucesso!');
+        this.router.navigate(['/cursos']); // Redireciona para a lista de cursos
+      });
+    } else {
+      console.log('Formulário inválido. Preencha todos os campos obrigatórios.');
+    }
+  }
+}
