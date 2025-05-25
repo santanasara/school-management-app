b4fed6c70469c52129e0cae211fc2a688b447db0
Joan Medeiros <joanmedeiros2018@gmail.com>
2025-05-25 13:35:44 -0300

    chore: adiciona variável de ambiente com a url da api do backend (#2)

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component'; // Seu componente raiz
-import { appConfig } from './app/app.config'; // <-- Importa e usa appConfig aqui
+import { AppComponent } from './app/app.component'; 
+import { appConfig } from './app/app.config'; 
 
-bootstrapApplication(AppComponent, appConfig) // <-- Deve usar appConfig
+bootstrapApplication(AppComponent, appConfig) 
   .catch(err => console.error(err));
