131dfba73ea759f622320da7ed252751c364c6ea
Joan Medeiros <joanmedeiros2018@gmail.com>
2025-05-25 23:27:40 -0300

    feat: refatorar componente para usar Angular Material e Tailwind CSS (#2)

--- frontend/src/app/modules/curso/components/curso-create/curso-create.component.ts
+++ frontend/src/app/modules/curso/components/curso-create/curso-create.component.ts
@@ -1,12 +1,14 @@
-// src/app/modules/curso/components/curso-create/curso-create.component.ts
-
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms'; // Para formulários reativos
-import { Router, RouterLink} from '@angular/router'; // Para navegação após a criação
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 import { CursoService } from '../../services/curso.service';
-import { Curso } from '../../models/curso'; 
+import { Curso } from '../../models/curso';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-curso-create',
@@ -14,19 +16,22 @@ import { Curso } from '../../models/curso';
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    RouterLink
+    RouterLink,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule
   ],
   templateUrl: './curso-create.component.html',
-  styleUrls: ['./curso-create.component.css']
 })
 export class CursoCreateComponent implements OnInit {
   cursoForm!: FormGroup;
 
   constructor(
-    private fb: FormBuilder, // Injeta FormBuilder para construir o formulário
+    private fb: FormBuilder,
     private cursoService: CursoService,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.cursoForm = this.fb.group({
@@ -42,7 +47,7 @@ export class CursoCreateComponent implements OnInit {
       const novoCurso: Curso = this.cursoForm.value;
       this.cursoService.createCurso(novoCurso).subscribe(() => {
         console.log('Curso criado com sucesso!');
-        this.router.navigate(['/cursos']); 
+        this.router.navigate(['/cursos']);
       });
     } else {
       console.log('Formulário inválido. Preencha todos os campos obrigatórios.');
