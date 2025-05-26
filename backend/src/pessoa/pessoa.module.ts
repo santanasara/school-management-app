595c53b8d7633c83eb5e9668eda061040eed5c1c
Sara Beatriz Mendonça de Santana <saraa.beatrizz@gmail.com>
2025-05-26 16:45:19 -0300

    feat: adds db queries for usuario e pessoa

--- backend/src/pessoa/pessoa.module.ts
+++ backend/src/pessoa/pessoa.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PessoaService } from './pessoa.service';
 import { PessoaController } from './pessoa.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Pessoa } from './entities/pessoa.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Pessoa])],
   controllers: [PessoaController],
   providers: [PessoaService],
 })
--- backend/src/usuario/entities/usuario.entity.ts
+++ backend/src/usuario/entities/usuario.entity.ts
@@ -1,9 +1,12 @@
 import { Matricula } from 'src/matricula/entities/matricula.entity';
+import { Pessoa } from 'src/pessoa/entities/pessoa.entity';
 import {
   Entity,
   PrimaryGeneratedColumn,
   Column,
   OneToMany,
+  JoinColumn,
+  OneToOne,
 } from 'typeorm';
 
 @Entity('usuario')
@@ -11,8 +14,9 @@ export class Usuario {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()  
-  pessoa_id: number;
+  @OneToOne(() => Pessoa) 
+  @JoinColumn() 
+  pessoa: Pessoa;
 
   @Column() 
   email: string;
