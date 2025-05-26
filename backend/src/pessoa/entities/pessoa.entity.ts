73c078d33e96ce69c8016d91604d66b8bde717a5
Sara Santana <42945235+santanasara@users.noreply.github.com>
2025-05-26 16:49:07 -0300

    feat: adds db queries for usuario e pessoa (#18)

--- backend/src/pessoa/entities/pessoa.entity.ts
+++ backend/src/pessoa/entities/pessoa.entity.ts
@@ -1,5 +1,13 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('pessoa') 
 export class Pessoa {
-    pessoa_id: number;
+    @PrimaryGeneratedColumn()
+    pessoa_id: number; 
+
+    @Column({ type: 'varchar', length: 255 })
     nome: string;
+    
+    @Column({ type: 'varchar', length: 11, unique: true })
     cpf: string;
 }
