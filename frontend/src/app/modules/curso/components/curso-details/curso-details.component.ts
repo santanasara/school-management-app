1c7c942440c38f97764a22ce4137353c6b6704fc
Joan Medeiros <joanmedeiros2018@gmail.com>
2025-05-25 23:30:14 -0300

    refactor: atualiza component curso-details (#2)

--- frontend/src/app/modules/curso/components/curso-details/curso-details.component.ts
+++ frontend/src/app/modules/curso/components/curso-details/curso-details.component.ts
@@ -1,35 +1,37 @@
-// src/app/modules/curso/components/curso-details/curso-details.component.ts
-
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute, RouterLink } from '@angular/router'; // Para obter parâmetros da URL
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { CursoService } from '../../services/curso.service';
-import { Curso } from '../../models/curso'; 
+import { Curso } from '../../models/curso';
 
+// Angular Material Modules
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-curso-details',
   standalone: true,
   imports: [
     CommonModule,
-    RouterLink // Para o link "Voltar para a lista"
+    RouterLink,
+    MatCardModule,
+    MatButtonModule
   ],
   templateUrl: './curso-details.component.html',
-  styleUrls: ['./curso-details.component.css']
 })
 export class CursoDetailsComponent implements OnInit {
   curso: Curso | undefined;
 
   constructor(
-    private route: ActivatedRoute, // Para acessar a rota atual
+    private route: ActivatedRoute,
     private cursoService: CursoService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       const idParam = params.get('id');
       if (idParam) {
-        const id = +idParam; // Converte string para número
+        const id = +idParam;
         this.cursoService.getCursoById(id).subscribe(data => {
           this.curso = data;
         });
