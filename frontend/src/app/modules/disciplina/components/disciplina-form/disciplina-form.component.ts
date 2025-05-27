8690c9ff6719dfb9d8a1ae79353c04d8e2b0006a
Joan Medeiros <joanmedeiros2018@gmail.com>
2025-05-27 16:10:18 -0300

    feat: implementa o component disciplina-form (#2)

---
+++ frontend/src/app/modules/disciplina/components/disciplina-form/disciplina-form.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { DisciplinaService } from '../../services/disciplina.service'; 
+import { Disciplina } from '../../models/disciplina.model'; 
+
+@Component({
+  selector: 'app-disciplina-form',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
+  templateUrl: './disciplina-form.component.html',
+})
+export class DisciplinaFormComponent implements OnInit {
+  private disciplinaService = inject(DisciplinaService);
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+
+  disciplina: Partial<Disciplina> = {};
+  editando = false;
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.editando = true;
+      this.disciplinaService.getById(+id).subscribe((d) => (this.disciplina = d));
+    }
+  }
+
+  salvar(): void {
+    if (this.editando && this.disciplina.id) {
+      this.disciplinaService.update(this.disciplina.id, this.disciplina).subscribe(() => this.router.navigate(['/disciplinas']));
+    } else {
+      this.disciplinaService.create(this.disciplina).subscribe(() => this.router.navigate(['/disciplinas']));
+    }
+  }
+}
