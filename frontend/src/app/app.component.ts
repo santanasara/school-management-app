ea811802bc9a95c82e0380b9c939ed1e07b19dcc
Joan Medeiros <joanmedeiros2018@gmail.com>
2025-05-27 16:12:02 -0300

    feat: adiciona o link para o módulo de disciplinas (#2)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { LayoutComponent } from './shared/components/layout/layout.component';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, LayoutComponent],
-  template: `<app-layout></app-layout>`,  // Renderiza o layout aqui
+  template: `<app-layout></app-layout>`,
 })
 export class AppComponent {}
