eebe2db4caa31d17ed5b943082c1aa58a77315f5
Joan Medeiros <joanmedeiros2018@gmail.com>
2025-05-27 21:47:26 -0300

    feat: define a interface model de Material no frontend (#2)

---
+++ frontend/src/app/modules/material/models/material.model.ts
@@ -0,0 +1,17 @@
+import { Disciplina } from "../../disciplina/models/disciplina.model";
+
+export enum TipoMaterial {
+  VIDEO = 'video',
+  PDF = 'pdf',
+  AUDIO = 'audio',
+  LINK = 'link',
+  OUTRO = 'outro',
+}
+
+export interface Material {
+  id: number;
+  titulo: string;
+  tipo: TipoMaterial;
+  link_arquivo: string;
+  disciplina: Disciplina | null;
+}
