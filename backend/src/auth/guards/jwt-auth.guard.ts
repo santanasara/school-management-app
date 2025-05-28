1ab5b7306c241e91f3e000b3602f1ce4d9845cbe
Sara Beatriz Mendonça de Santana <saraa.beatrizz@gmail.com>
2025-05-28 15:00:51 -0300

    feat: adds authorization by role

--- backend/src/auth/guards/jwt-auth.guard.ts
+++ backend/src/auth/guards/jwt-auth.guard.ts
@@ -2,4 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+    async validate(payload: any) {
+        return {
+            userId: payload.sub,
+            username: payload.username,
+            perfil: payload.perfil
+        };
+    }
+}
