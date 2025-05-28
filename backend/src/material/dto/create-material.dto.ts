c7c08ef09e3e2fd71121bdcef3004bfa7cdda45c
Joan Medeiros <joanmedeiros2018@gmail.com>
2025-05-27 21:22:14 -0300

    feat: define entidade material (#2)

--- backend/src/material/dto/create-material.dto.ts
+++ backend/src/material/dto/create-material.dto.ts
@@ -1,1 +1,17 @@
-export class CreateMaterialDto {}
+import { IsString, IsOptional, IsEnum, IsInt } from 'class-validator';
+import { TipoMaterial } from '../entities/tipo-material.enum';
+
+export class CreateMaterialDto {
+  @IsString()
+  titulo: string;
+
+  @IsEnum(TipoMaterial)
+  tipo: TipoMaterial;
+
+  @IsString()
+  link_arquivo: string;
+
+  @IsOptional()
+  @IsInt()
+  disciplina_id?: number;
+}
--- backend/src/material/entities/material.entity.ts
+++ backend/src/material/entities/material.entity.ts
@@ -1,1 +1,25 @@
-export class Material {}
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { TipoMaterial } from './tipo-material.enum';
+import { Disciplina } from 'src/disciplina/entities/disciplina.entity';
+
+@Entity()
+export class Material {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  titulo: string;
+
+  @Column({
+    type: 'enum',
+    enum: TipoMaterial,
+    default: TipoMaterial.OUTRO,
+  })
+  tipo: TipoMaterial;
+
+  @Column()
+  link_arquivo: string;
+
+  @ManyToOne(() => Disciplina, { nullable: true, onDelete: 'CASCADE' })
+  disciplina: Disciplina | null;
+}
