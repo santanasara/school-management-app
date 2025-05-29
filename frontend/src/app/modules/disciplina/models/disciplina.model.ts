c73f3ab7e38bfedf627d0887413ab27289c8eb40
Joan Medeiros <joanmedeiros2018@gmail.com>
2025-05-29 16:02:44 -0300

    refactor: corrije o form de disciplina (#2)

--- frontend/src/app/modules/disciplina/models/disciplina.model.ts
+++ frontend/src/app/modules/disciplina/models/disciplina.model.ts
@@ -1,10 +1,11 @@
+import { Curso } from "../../curso/models/curso.model";
 import { Turma } from "../../turma/models/turma.model";
-import { Curso } from "../../curso/models/curso.model"; 
 
 export interface Disciplina {
   id: number;
   nome: string;
   descricao?: string;
-  curso: Curso;              
+  curso: Partial<Curso>; 
+  cursoId?: number;
   turmas?: Turma[];
 }
