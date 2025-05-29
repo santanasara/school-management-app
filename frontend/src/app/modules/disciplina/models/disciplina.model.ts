74a986c43536929265176029c6a7a37ce6ec09e8
Joan Medeiros <joanmedeiros2018@gmail.com>
2025-05-29 14:21:20 -0300

    refactor: atualiza interface disciplina (#2)

--- frontend/src/app/modules/disciplina/models/disciplina.model.ts
+++ frontend/src/app/modules/disciplina/models/disciplina.model.ts
@@ -1,8 +1,10 @@
 import { Turma } from "../../turma/models/turma.model";
+import { Curso } from "../../curso/models/curso.model"; 
 
 export interface Disciplina {
   id: number;
   nome: string;
   descricao?: string;
+  curso: Curso;              
   turmas?: Turma[];
 }
