ea6832009345078e58d0f36ef06118879ccc0bd4
Sara Beatriz Mendonça de Santana <saraa.beatrizz@gmail.com>
2025-05-29 13:20:51 -0300

    feat: adds authorization to frontend

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,15 +1,20 @@
-// src/app/app.config.ts
-
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http'; // <-- Importe provideHttpClient aqui!
-
-import { routes } from './app.routes'; 
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { routes } from './app.routes';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './modules/auth/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient() 
+    provideHttpClient(
+      withInterceptorsFromDi()
+    ),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
   ]
 };
